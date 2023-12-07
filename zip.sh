9151ab3e205c8bf400a32092b015063a7cce2a1d
jmcb <joelsgp@protonmail.com>
2023-12-07 18:12:46 +0000

    Remove old bit of script

--- zip.sh
+++ zip.sh
@@ -6,7 +6,7 @@ bundle_file='git.bundle'
 zip_file="${1:-exam.zip}"
 
 git bundle create "${bundle_file}" "${git_ref}"
-git archive --output="${zip_file}" --add-file="${bundle_file}" "${git_ref}" "${include[@]}"
+git archive --output="${zip_file}" --add-file="${bundle_file}" "${git_ref}"
 
 ls -lh "${zip_file}"
 md5sum "${zip_file}"
