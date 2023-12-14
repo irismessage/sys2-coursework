454dd380992878cd8ba9cbbc248702512bea21b7
jm3017 <jm3017@york.ac.uk>
2023-12-14 13:03:03 +0000

    Messed up port thing

--- question10/ftp_port.py
+++ question10/ftp_port.py
@@ -7,9 +7,11 @@ else:
     input_string = argv[1]
 
 input_list = input_string.strip("() ").split(",")
-ip_addr = ".".join(input_list[0:4])
+ip_addr_list = input_list[0:4]
+ip_addr = ".".join(ip_addr_list)
 port_high = int(input_list[4]) * 256
 port_low = int(input_list[5])
+port = port_high + port_low
 
-output = f"telnet {ip_addr} {port_high}{port_low}"
+output = f"telnet {ip_addr} {port}"
 print(output)
