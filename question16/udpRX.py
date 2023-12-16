f3e5d01d94b7f5c94f9537a21da1fa4b55aeab57
jm3017 <jm3017@york.ac.uk>
2023-12-16 21:02:59 +0000

    Rename sockets

--- question16/udpRX.py
+++ question16/udpRX.py
@@ -7,13 +7,13 @@ UDP_PORT = 8080
 # BUF_SIZE = 1024
 BUF_SIZE = 9000
 
-sockRX = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sockRX.bind((UDP_IP, UDP_PORT))
+sock_rx = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_rx.bind((UDP_IP, UDP_PORT))
 
-data, addr = sockRX.recvfrom(BUF_SIZE)
+data, addr = sock_rx.recvfrom(BUF_SIZE)
 file_name = data.strip()
 
-data, addr = sockRX.recvfrom(BUF_SIZE)
+data, addr = sock_rx.recvfrom(BUF_SIZE)
 number_of_segments = int(data)
 
 print("RX File: {0}".format(file_name))
@@ -22,17 +22,17 @@ f = open(file_name, "wb")
 
 try:
     for i in range(0, number_of_segments):
-        sockRX.settimeout(2)
-        data, addr = sockRX.recvfrom(BUF_SIZE)
+        sock_rx.settimeout(2)
+        data, addr = sock_rx.recvfrom(BUF_SIZE)
         f.write(data)
         print("rx segment: " + str(i))
     print("Download complete")
     f.close()
-    sockRX.close()
+    sock_rx.close()
     image = Image.open(file_name)
     image.show()
 
 except socket.timeout:
     print("Oops timeout")
     f.close()
-    sockRX.close()
+    sock_rx.close()
