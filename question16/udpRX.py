2a277c1021798e6dcf6cb163d0f4a6db78f8bead
jm3017 <jm3017@york.ac.uk>
2023-12-16 20:59:57 +0000

    Apply formatter

--- question16/udpRX.py
+++ question16/udpRX.py
@@ -1,4 +1,5 @@
 import socket
+
 from PIL import Image
 
 UDP_IP = "127.0.0.1"
@@ -6,34 +7,32 @@ UDP_PORT = 8080
 # BUF_SIZE = 1024
 BUF_SIZE = 9000
 
-sockRX = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+sockRX = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sockRX.bind((UDP_IP, UDP_PORT))
 
-data,addr = sockRX.recvfrom(BUF_SIZE)
+data, addr = sockRX.recvfrom(BUF_SIZE)
 file_name = data.strip()
 
-data,addr = sockRX.recvfrom(BUF_SIZE)
+data, addr = sockRX.recvfrom(BUF_SIZE)
 number_of_segments = int(data)
 
 print("RX File: {0}".format(file_name))
 
-f = open(file_name,'wb')
+f = open(file_name, "wb")
 
 try:
-  for i in range(0,number_of_segments):
-    sockRX.settimeout(2)
-    data,addr = sockRX.recvfrom(BUF_SIZE)
-    f.write(data)
-    print("rx segment: " + str(i))
-  print("Download complete")
-  f.close()
-  sockRX.close()
-  image = Image.open(file_name)
-  image.show()
+    for i in range(0, number_of_segments):
+        sockRX.settimeout(2)
+        data, addr = sockRX.recvfrom(BUF_SIZE)
+        f.write(data)
+        print("rx segment: " + str(i))
+    print("Download complete")
+    f.close()
+    sockRX.close()
+    image = Image.open(file_name)
+    image.show()
 
 except socket.timeout:
-  print("Oops timeout")
-  f.close()
-  sockRX.close()
-  
-
+    print("Oops timeout")
+    f.close()
+    sockRX.close()
--- question16/udpTX.py
+++ question16/udpTX.py
@@ -1,5 +1,5 @@
-import socket
 import os
+import socket
 import time
 
 UDP_IP = "127.0.0.1"
@@ -7,12 +7,12 @@ UDP_PORT = 8080
 # BUF_SIZE = 1024
 BUF_SIZE = 9000
 
-sockTX = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+sockTX = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-read_file_name="bob.jpg"
-write_file_name="bob-copy.jpg"
+read_file_name = "bob.jpg"
+write_file_name = "bob-copy.jpg"
 file_size = os.path.getsize(read_file_name)
-number_of_segments = (-(-file_size//BUF_SIZE))
+number_of_segments = -(-file_size // BUF_SIZE)
 
 sockTX.sendto(write_file_name.encode("ascii"), (UDP_IP, UDP_PORT))
 time.sleep(0.1)
@@ -21,15 +21,14 @@ time.sleep(0.1)
 
 print("Sending: " + read_file_name + ":" + str(number_of_segments))
 
-f=open(read_file_name,"rb")
+f = open(read_file_name, "rb")
 data = f.read(BUF_SIZE)
 
-for i in range(0,number_of_segments):
-  sockTX.sendto(data, (UDP_IP, UDP_PORT))
-  time.sleep(0.1)
-  print("tx segment: " + str(i) + " len: " + str(len(data)))
-  data = f.read(BUF_SIZE)
+for i in range(0, number_of_segments):
+    sockTX.sendto(data, (UDP_IP, UDP_PORT))
+    time.sleep(0.1)
+    print("tx segment: " + str(i) + " len: " + str(len(data)))
+    data = f.read(BUF_SIZE)
 
 sockTX.close()
 f.close()
-
