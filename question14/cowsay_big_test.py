7d892944fa4ccb1dfee85ef746841556262d3bbd
jm3017 <jm3017@york.ac.uk>
2023-12-16 17:55:45 +0000

    format file

--- question14/cowsay_big_test.py
+++ question14/cowsay_big_test.py
@@ -2,52 +2,56 @@ import socket
 from pathlib import Path
 
 
-TCP_IP = "192.168.100.1"       
+TCP_IP = "192.168.100.1"
 TCP_PORT = 1234
 BUF_SIZE = 1024
-SIZE = 8000                 # UPDATE
+SIZE = 8000  # UPDATE
 DATA_FILE_PATH = Path(__file__).parent.joinpath("pg164.txt")
 
 sockTX = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sockTX.connect((TCP_IP, TCP_PORT))
-sockTX.settimeout(2.0) 
+sockTX.settimeout(2.0)
 
-dataFile = open(DATA_FILE_PATH, 'r')
-try: 
-  print("RX:" + sockTX.recv(BUF_SIZE).decode('UTF-8') )  
-  print("TX: helo")
-  sockTX.send(b"helo")
-  print("RX:" + sockTX.recv(BUF_SIZE).decode('UTF-8') )  
+dataFile = open(DATA_FILE_PATH, "r")
+try:
+    print("RX:" + sockTX.recv(BUF_SIZE).decode("UTF-8"))
+    print("TX: helo")
+    sockTX.send(b"helo")
+    print("RX:" + sockTX.recv(BUF_SIZE).decode("UTF-8"))
 
-  data=[]    
-  while (len(data) < SIZE):
-    text = dataFile.read(1)
-    if text != '':
-      if (ord(text)>0x2F and ord(text)<0x3A) or (ord(text)>0x40 and ord(text)<0x5B) or (ord(text)==0x20) or (ord(text)>0x60 and ord(text)<0x7B):
-        data.append( text )
-      else:
-        data.append( " " )
+    data = []
+    while len(data) < SIZE:
+        text = dataFile.read(1)
+        if text != "":
+            if (
+                (ord(text) > 0x2F and ord(text) < 0x3A)
+                or (ord(text) > 0x40 and ord(text) < 0x5B)
+                or (ord(text) == 0x20)
+                or (ord(text) > 0x60 and ord(text) < 0x7B)
+            ):
+                data.append(text)
+            else:
+                data.append(" ")
 
-  sockTX.send( b"text" ) 
-  sockTX.send( ''.join(str(x) for x in data).encode('UTF-8') )
-  sockTX.send( b"\n" ) 
+    sockTX.send(b"text")
+    sockTX.send("".join(str(x) for x in data).encode("UTF-8"))
+    sockTX.send(b"\n")
 
-  try:
-    while True:
-      data = sockTX.recv(BUF_SIZE)
-      if not data:
-        break
+    try:
+        while True:
+            data = sockTX.recv(BUF_SIZE)
+            if not data:
+                break
 
-      #print("### " + str(len(data)) + " ###")	
-      print(data.decode('UTF-8'))
-  except socket.timeout:
-    pass
+            # print("### " + str(len(data)) + " ###")
+            print(data.decode("UTF-8"))
+    except socket.timeout:
+        pass
 
-  sockTX.send( b"quit\n" )   
-  print("RX:" + sockTX.recv(BUF_SIZE).decode('UTF-8'))
-  sockTX.close()
+    sockTX.send(b"quit\n")
+    print("RX:" + sockTX.recv(BUF_SIZE).decode("UTF-8"))
+    sockTX.close()
 
-except KeyboardInterrupt:  
-  sockTX.close()
-  print("Exit")
-
+except KeyboardInterrupt:
+    sockTX.close()
+    print("Exit")
