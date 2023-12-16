8208221324f60d81ca7bbf748571f6062903c6d3
jm3017 <jm3017@york.ac.uk>
2023-12-16 18:03:38 +0000

    Load text first

--- question14/cowsay_big_test.py
+++ question14/cowsay_big_test.py
@@ -8,33 +8,35 @@ BUF_SIZE = 1024
 SIZE = 8000  # UPDATE
 DATA_FILE_PATH = Path(__file__).parent.joinpath("pg164.txt")
 
+
+data = []
+with open(DATA_FILE_PATH, "r") as data_file:
+    while len(data) < SIZE:
+        char = data_file.read(1)
+        if char != "":
+            if (
+                (ord(char) > 0x2F and ord(char) < 0x3A)
+                or (ord(char) > 0x40 and ord(char) < 0x5B)
+                or (ord(char) == 0x20)
+                or (ord(char) > 0x60 and ord(char) < 0x7B)
+            ):
+                data.append(char)
+            else:
+                data.append(" ")
+text = "".join(data)
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((TCP_IP, TCP_PORT))
 sock.settimeout(2.0)
 
-data_file = open(DATA_FILE_PATH, "r")
 try:
     print("RX:" + sock.recv(BUF_SIZE).decode("UTF-8"))
     print("TX: helo")
     sock.send(b"helo")
     print("RX:" + sock.recv(BUF_SIZE).decode("UTF-8"))
 
-    data = []
-    while len(data) < SIZE:
-        text = data_file.read(1)
-        if text != "":
-            if (
-                (ord(text) > 0x2F and ord(text) < 0x3A)
-                or (ord(text) > 0x40 and ord(text) < 0x5B)
-                or (ord(text) == 0x20)
-                or (ord(text) > 0x60 and ord(text) < 0x7B)
-            ):
-                data.append(text)
-            else:
-                data.append(" ")
-
     sock.send(b"text")
-    sock.send("".join(str(x) for x in data).encode("UTF-8"))
+    sock.send(text.encode("UTF-8"))
     sock.send(b"\n")
 
     try:
