b3acb4cb34957b4ff7224bb01a15d09f75d50f63
jmcb <joelsgp@protonmail.com>
2024-01-18 11:13:39 +0000

    Copy fortune.py to root dir just to be safe with compliance

--- image_embeds.py
+++ image_embeds.py
@@ -4,7 +4,7 @@ workdir = Path()
 for folder in sorted(workdir.glob("question*/")):
     if not folder.is_dir():
         continue
-    
+
     print("### Screenshots")
     for screenshot in sorted(folder.iterdir()):
         if not screenshot.suffix == ".png":
